749521ecb7502160d1b190b228e6b5a342cce13b
Jarrod Millman <jarrod.millman@gmail.com>
2011-08-20 23:29:13 -0700

    use new configuration to order articles in proceedings

--- publisher/conf.py
+++ publisher/conf.py
@@ -3,4 +3,24 @@ import os
 
 output_dir = 'output'
 cover_dir = 'cover_material'
-dirs = sorted([d.split('/')[1] for d in glob.glob('%s/*' % output_dir) if os.path.isdir(d)])
+#dirs = sorted([d.split('/')[1] for d in glob.glob('%s/*' % output_dir) if os.path.isdir(d)])
+dirs = [
+   'vanderwalt',
+   'bergstra',
+   'meinke',
+   'ramachandran',
+   'colbert',
+   'ragan-kelley',
+   'aldcroft',
+   'hack',
+   'morley',
+   'greenfield',
+   'mckinney',
+   'seabold',
+   'arnold',
+   'reikeras',
+   'devarajan',
+   'overholt',
+   'henderson',
+   'floehr'
+]
