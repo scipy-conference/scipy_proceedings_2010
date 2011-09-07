21aa0e88351f2df0f2abd9e4b1434e4b6d779848
Jarrod Millman <jarrod.millman@gmail.com>
2011-09-07 12:13:43 -0700

    to avoid merge conflict

--- publisher/conf.py
+++ publisher/conf.py
@@ -3,24 +3,4 @@ import os
 
 output_dir = 'output'
 cover_dir = 'cover_material'
-#dirs = sorted([d.split('/')[1] for d in glob.glob('%s/*' % output_dir) if os.path.isdir(d)])
-dirs = [
-   'vanderwalt',
-   'bergstra',
-   'meinke',
-   'ramachandran',
-   'colbert',
-   'ragan-kelley',
-   'aldcroft',
-   'hack',
-   'morley',
-   'greenfield',
-   'mckinney',
-   'seabold',
-   'arnold',
-   'reikeras',
-   'devarajan',
-   'overholt',
-   'henderson',
-   'floehr'
-]
+dirs = sorted([d.split('/')[1] for d in glob.glob('%s/*' % output_dir) if os.path.isdir(d)])
